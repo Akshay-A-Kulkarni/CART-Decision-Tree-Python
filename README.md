6869b12c4c624ba0a462860a71e1cf241a7018a1
Akshay Kulkarni <akshayavinashkulkarni@gmail.com>
2019-12-23 19:41:50 -0500

    pushed to remote repo

---
+++ README.md
@@ -0,0 +1,3 @@
+Decision Tree using Gini Impurity.
+
+Implementation of a decision tree classifier from scratch
---
+++ decisiontree.py
@@ -0,0 +1,101 @@
+# Decision Tree Implementation
+
+# Creating class to represent node in tree
+class Node:
+    def __init__(self, predicted_class):
+        # Setting Node Properties
+        self.predicted_class = predicted_class
+        self.feature_index = 0
+        self.threshold = 0
+        self.left = None
+        self.right = None
+
+# Classifier Class
+class DecisionTreeClassifier:
+    def __init__(self, max_depth=None):
+        self.max_depth = max_depth
+        
+    def fit(self, X, y, w=None):
+        # Init variables
+        self.n_classes_ = len(set(y))
+        self.n_features_ = X.shape[1]
+        self.tree_ = self._grow_tree(X, y)
+
+    def predict(self, X):
+        return np.array([self._predict(inputs) for inputs in X])
+
+    def _best_split(self, X, y):
+        '''
+        Method to find the best split for a node using Gini Impurity
+        '''
+        m = y.size
+        if m <= 1:
+            return None, None
+        num_parent = [np.sum(y == c) for c in range(self.n_classes_)]
+        best_gini = 1.0 - sum((n / m) ** 2 for n in num_parent)
+        best_idx, best_thr = None, None
+        for idx in range(self.n_features_):
+            thresholds, classes = zip(*sorted(zip(X[:, idx], y)))
+            num_left = [0] * self.n_classes_
+            num_right = num_parent.copy()
+            for i in range(1, m):
+                c = classes[i - 1]
+                num_left[int(c)] += 1
+                num_right[int(c)] -= 1
+                gini_left = 1.0 - sum(
+                    (num_left[x] / i) ** 2 for x in range(self.n_classes_)
+                )
+                gini_right = 1.0 - sum(
+                    (num_right[x] / (m - i)) ** 2 for x in range(self.n_classes_)
+                )
+                gini = (i * gini_left + (m - i) * gini_right) / m
+                if thresholds[i] == thresholds[i - 1]:
+                    continue
+                if gini < best_gini:
+                    best_gini = gini
+                    best_idx = idx
+                    best_thr = (thresholds[i] + thresholds[i - 1]) / 2
+        return best_idx, best_thr
+
+    def _grow_tree(self, X, y, depth=0):
+        num_samples_per_class = [np.sum(y == i) for i in range(self.n_classes_)]
+        predicted_class = np.argmax(num_samples_per_class)
+        node = Node(predicted_class=predicted_class)
+        if depth < self.max_depth:
+            idx, thr = self._best_split(X, y)
+            if idx is not None:
+                indices_left = X[:, idx] < thr
+                X_left, y_left = X[indices_left], y[indices_left]
+                X_right, y_right = X[~indices_left], y[~indices_left]
+                node.feature_index = idx
+                node.threshold = thr
+                node.left = self._grow_tree(X_left, y_left, depth + 1)
+                node.right = self._grow_tree(X_right, y_right, depth + 1)
+        return node
+
+    def _predict(self, inputs):
+        node = self.tree_
+        while node.left:
+            if inputs[node.feature_index] < node.threshold:
+                node = node.left
+            else:
+                node = node.right
+        return node.predicted_class
+
+
+def main():
+    Xq = features.to_numpy()    
+    yq = response.to_numpy()
+        
+    X_train, X_test, y_train, y_test= train_test_split(Xq,yq, test_size = 0.25, random_state = 123, stratify=yq)
+    # Changing labels to 0 to -1 for adaboost
+    y_test[y_test == 0] = -1
+    
+    dclf = DecisionTreeClassifier(max_depth=1)    
+    dclf.fit(X_train, y_train)
+    pred = dclf.predict(X_test)
+    pred[pred == 0] = -1
+
+    print("Scratch DT with depth-1 Accuracy:",metrics.accuracy_score(y_test, pred))
+    
+main()
